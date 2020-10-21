f585b9fc216696d093bed24e848c3f1e9f09d52f
Lorenzo Fasolino <fasolinolorenzo@gmail.com>
2020-10-21 18:34:37 +0200

    automation

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
+
 driver = webdriver.Chrome("./chromedriver")
-driver.get("https://www.nytimes.com")
-headlines = driver.find_elements_by_class_name("story-heading")
-for headline in headlines:
-    print(headline.text.strip())
+
+with open("Top 50 Alexa sites/top-1m.csv") as reader:
+
+    line = reader.readline()
+    while line != '':
+        driver.get("http://"+line)
+        headlines = driver.find_elements_by_class_name("story-heading")
+        for headline in headlines:
+            print(headline.text.strip())
+        line = reader.readline()
+
+
+
