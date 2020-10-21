1447bb604002bab2e3fd9c5a04a2f66eeb2ee3e2
Lorenzo Fasolino <fasolinolorenzo@gmail.com>
2020-10-21 17:37:20 +0200

    Modificati i permessi

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-driver = webdriver.Chrome("/Users/roberta/Documents/1.Università/RetiGeografiche/Task/3/chromedriver")
+driver = webdriver.Chrome("./chromedriver")
 driver.get("https://www.nytimes.com")
 headlines = driver.find_elements_by_class_name("story-heading")
 for headline in headlines:
