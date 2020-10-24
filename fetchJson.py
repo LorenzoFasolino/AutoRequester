7f2f41ddfc47b4078cf40b1219610fa302b7f9c3
Mario <39814508+Marioegidio@users.noreply.github.com>
2020-10-24 15:27:28 +0200

    elco siti http ok

--- fetchJson.py
+++ fetchJson.py
@@ -2,32 +2,44 @@ import sys
 import urllib
 import json
 
+PORTA_HTTPS = "443"
+PORTA_HTTP = "80"
 url = sys.argv[-1]
 countHttps = 0
 countHttp = 0
 countHttpDue = 0
 countHttpDueSettings = 0
 data = None
+elencoHttp = set()
 i = 0
+
 with open(url, encoding='utf-8') as file:
     data = json.load(file)
     for d in data:
+        
         tcpDstport = d['_source']['layers']['tcp']['tcp.dstport']
+        contenutoLayers = d['_source']['layers']
 
-        if tcpDstport == "443":
+        #contro i siti http e https
+        if tcpDstport == PORTA_HTTPS:
             countHttps += 1
-        elif tcpDstport == "80":
+        elif tcpDstport == PORTA_HTTP:
             countHttp += 1
         else:
             print(tcpDstport + "\n")
 
-        if ('http2' in d['_source']['layers']):
+        # prelevo tutti i siti visitati
+        if ('http2' in contenutoLayers):
             countHttpDue += 1
-            
-            # if len(d['_source']['layers']['http2'].keys()) > 1:
+            # cerco  "http2.header.name": "referer",
 
+        elif ('http' in contenutoLayers):
+            # cerco  "http.host":
+            elencoHttp.add(d['_source']['layers']['http']['http.host'])
+        
+        # pacchetti non meglio identificati
+            # if len(d['_source']['layers']['http2'].keys()) > 1:
             #     print("ciaoooo"+str(d['_source']['layers']['http2'].keys()[1]))
-           
             #     for nonLoSo in list(d['_source']['layers']['http2'].items()):
             #         # print(str(d['_source']['layers']['http2']['http2.stream'])+"\n")
             #         #print(str(nonLoSo))
@@ -37,10 +49,7 @@ with open(url, encoding='utf-8') as file:
             #             countHttpDueSettings += 1
             #     break
 
-            
-        ip = d['_source']['layers']['ip']['ip.dst']
-        #print(str(i)+ ") " +ip)
-        i += 1
+
 
 
 print("\n\nI pacchetti in totale sono " + str(len(data)) + "\n")
@@ -50,6 +59,5 @@ print("I pacchetti http2 Settings sono " + str(countHttpDueSettings) + "\n")
 print("I pacchetti http che non usano ssl sono " +
       str(countHttp) + " (" + (str(len(data)-countHttps)) + ")\n")
 
-
-#              "http2.header.name": "referer",
-#        "http.host":
+# stampo i siti http
+# print(str(elencoHttp) )
